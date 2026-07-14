40dc4fe19b0a5c66d33087d7680e8a3db4d666b4
burhanuddin2026 <sarahdubaiwala90@gmail.com>
2026-07-14 17:53:16 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,6 @@
+side_length = 4 # You can change this number for different sizes
+
+# Outer loop for rows
+for i in range(side_length):
+    # Inner loop for columns, prints stars in each row
+    print("*" * side_length)
